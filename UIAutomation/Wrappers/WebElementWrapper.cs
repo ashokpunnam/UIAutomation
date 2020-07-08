f2071001c06501aa6695b691535c8a18d81f11e7
ashokpunnam <Ashok.Punnam@aa.com>
2020-07-08 00:05:09 -0500

    Added Framework components - BasePage and DriverContext
    Below 2 class files are added to Framework project:
    BasePage: BasePage to handle common functionality of all Pages.
    DriverContext: DriverContext is added to instantiate and set driver object that can be available across all pages in a session.

---
+++ UIAutomation/Wrappers/WebElementWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UIAutomationFramework.Wrappers
+{
+   public static class WebElementWrapper
+    {
+        public static void executeJavaScript()
+        {
+
+
+        }
+
+    }
+}
