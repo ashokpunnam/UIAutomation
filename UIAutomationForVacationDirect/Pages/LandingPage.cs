0c46e642a7d389f7e6b43800b5e98421d20bfb8e
ashokpunnam <Ashok.Punnam@aa.com>
2020-07-07 21:09:13 -0500

    Designed and added Framework Components
    Identified framework components required and added them in this commit. Below are the framework components added:
    1. Base classes
    2. Configurations
    3. Utilities -->Excel, Log, Report etc.
    4. Wrapper classes for element wrappers and driver wrapper
    
    Also created Pages folder and base page classes

---
+++ UIAutomationForVacationDirect/Pages/LandingPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIAutomationForVacationDirect.Pages
+{
+    class LandingPage
+    {
+        private By btnHotel = By.XPath("//*[@id='SearchWidgetWrapper']/section/div/ul/li[2]/button");
+    }
+}
