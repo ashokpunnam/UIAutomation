71f6611c6dc056112e9679607622ccb968fec810
ashokpunnam <Ashok.Punnam@aa.com>
2020-07-11 18:52:27 -0500

    Base class and base unit test case is created
    - Base class is created which acts as a base for all the test cases. It initializes objects like driver, browser etc. required to launch a browser and open an application
    - Basic unit test case is created to instantiate pages and execute the functionality
    - New package 'nunit'  is added to the Test project

---
+++ UIAutomationForVacationDirect/HookInitialize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIAutomationFramework.Base;
+using static UIAutomationFramework.Base.Browser;
+
+namespace UIAutomationForVacationDirect
+{
+   public class HookInitialize:TestInitializeHook
+    {
+        public HookInitialize():base(BrowserType.Chrome)
+        {
+            InitializeSettings();
+
+        }
+
+    }
+}
---
+++ UIAutomationForVacationDirect/TestCases/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using UIAutomationForVacationDirect.Pages;
+using UIAutomationFramework.Base;
+
+namespace UIAutomationForVacationDirect
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       // public IWebDriver driver;   
+       
+        [SetUp]
+        public void SetupTest()
+        {
+            new HookInitialize();
+
+        }
+        
+        [Test]
+        public void TestMethod()
+        {
+            LandingPage lp = new LandingPage();
+            HotelSearchPage hsp = lp.clickHotel();
+            HotelSearchResultsPage hsrp = hsp.SearchForHotels("New York City", 0, 29);
+            int[] filter = { 5 };
+            hsrp.filterHotelResults(filter);
+            //Thread.Sleep(10000);
+            HotelBookRoomPage hbrp = hsrp.chooseRoom();
+            int noOfRooms = hbrp.listOfAvailableRooms();
+            Console.WriteLine("RoomsAvailable" + noOfRooms.ToString());
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            Thread.Sleep(10000);
+            BasePage basePage = new BasePage();
+            basePage.TearDown();
+
+        }
+    }
+}
