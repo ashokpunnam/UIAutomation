7aa228965e4324a0f6643480a531a2f265341e05
ashokpunnam <Ashok.Punnam@aa.com>
2020-07-11 19:08:50 -0500

    DriverContext, BrowserContext, Base test class and TestInitilizer classes added
    Below 4 important framework components are added
    - Base. This is the parent class of Base Page and define the common behaviors required for all the pages and all the test cases
    - Browser. Browser class define Browser property and how to get/set browser property for different browser types: Chrome, Firefox, IE etc.
    - DriverContext . This class define and produces Driver object required to execute all test cases in a session based on the browser type passes to it as an argument.
    - TestInitialize - This is the starting point of test execution and it is used to initialize all framework components

---
+++ UIAutomation/Base/TestInitializeHook.cs
@@ -0,0 +1,72 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Text;
+using UIAutomationFramework.Config;
+using UIAutomationFramework.Utilities;
+using static UIAutomationFramework.Base.Browser;
+
+namespace UIAutomationFramework.Base
+{
+    public class TestInitializeHook : Base
+    {
+        public readonly BrowserType Browser;
+
+        public TestInitializeHook(BrowserType browser)
+        {
+            Browser = browser;            
+        }
+
+
+        public void InitializeSettings()
+        {
+            //Set all the settings for framework
+            //ConfigReader.SetFrameworkSettings();
+
+            //Set Log
+            //LogHelpers.CreateLogFile();
+
+            //Open Browser
+            OpenBrowser(Browser);
+            NavigateSite();
+            //LogHelpers.Write("Initialized framework");
+
+        }
+
+        private void OpenBrowser(BrowserType browserType = BrowserType.FireFox)
+        {
+            switch (browserType)
+            {
+                case BrowserType.InternetExplorer:
+                    DriverContext.Driver = new InternetExplorerDriver();
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+                case BrowserType.FireFox:
+                    DriverContext.Driver = new FirefoxDriver();
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+                case BrowserType.Chrome:
+                    DriverContext.Driver = new ChromeDriver();
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+            }
+
+        }
+
+        public virtual void NavigateSite()
+        {
+            // DriverContext.Browser.GoToUrl(Settings.AUT);   
+            DriverContext.Browser.GoToUrl("http://www.vacationsdirect.com");
+
+
+            // .Manage().Timeouts(10);
+//            LogUtility.Write("Opened the browser !!!");
+        }
+
+
+    }
+
+
+
+}
